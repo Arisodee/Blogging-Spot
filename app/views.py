e0aeceefab2118c23622837d4468080074f38dd0
Arisodee <arisodee@gmail.com>
2020-12-14 11:19:47 +0300

    Add login details

--- app/views.py
+++ app/views.py
@@ -50,11 +50,17 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login',methods = ['GET', 'POST'])
 def login():
 
     '''
     View root page function that returns the about page and its data
     '''
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
